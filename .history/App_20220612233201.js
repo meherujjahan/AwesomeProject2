e1620a9f86720719786d497c83c090917ccf99dd
meherujjahan <jikrarjahan441@gmail.com>
2022-06-12 23:32:12 +0600

    add

--- .history/App_20220612233201.js
+++ .history/App_20220612233201.js
@@ -1,7 +1,11 @@
+import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { useFonts } from 'expo-font';
+import { StatusBar } from 'expo-status-bar';
 import { StyleSheet } from 'react-native';
 import Text from './src/components/text/text';
+import Details from './src/screens/details';
+import Home from './src/screens/home';
 
 
 
@@ -20,7 +24,15 @@ if(!loaded){
 //navigation system
 const Stack = createNativeStackNavigator();
   return (
-  
+   <>
+       <NavigationContainer>
+      <Stack.Navigator  screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Details" component={Details} />
+      </Stack.Navigator>
+    </NavigationContainer>
+    <StatusBar style='light'/>
+    </>
    
   );
 }
